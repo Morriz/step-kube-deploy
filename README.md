82abec85b6a6d429769093d125c872e01a120ac8
Maurice Faber <morriz@idiotz.nl>
2016-04-06 06:33:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# kube-deploy
+
+This step uses the kubectl executable to replace a [kubernetes deployment](http://kubernetes.io/docs/user-guide/deployments/).
+Authentication options are passed along to the `kubectl` executable as is.
+
+# Options:
+
+- `deployment` The name of the deployment that needs to be updated.
+- `tag` The tag of the image for the rolling update.
+- `debug` (optional, default: `false`) List the kubectl before running it.
+Warning, all environment variables are expanded, including the password.
+- `raw-global-args` (optional) Arguments that are placed before `kubectl replace`.
+
+## Kubectl flags
+
+The following options are available as wercker properties. The values are passed
+directly to the `kubectl` command. See the `kubectl` for the documentation.
+
+- `insecure-skip-tls-verify`
+- `password`
+- `server`
+- `token`
+- `username`
+
+If a flag is not available, use the `raw-global-args`.
+
+# Example
+
+```
+deploy:
+    steps:
+      - morriz/step-kube-deploy:
+          server: $KUBERNETES_MASTER
+          username: $KUBERNETES_USERNAME
+          password: $KUBERNETES_PASSWORD
+          insecure-skip-tls-verify: true
+          deployment: create -f cities-controller.json
+```
+
+# License
+
+The MIT License (MIT)
+
+# Changelog
+
+## 1.1.0
+
+- Update to kubectl to version `1.1.3`
+
+## 1.0.0
+
+- Initial release
---
+++ run.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+#fail='fail'
+#info='info'
+fail='echo'
+info='echo'
+
+main() {
+  display_version
+
+  if [ -z "$WERCKER_KUBE_DEPLOY_DEPLOYMENT" ]; then
+    $fail "wercker-kube-deploy: deployment argument cannot be empty"
+    exit
+  fi
+
+  if [ -z "$WERCKER_KUBE_DEPLOY_TAG" ]; then
+    $fail "wercker-kube-deploy: tag argument cannot be empty"
+    exit
+  fi
+
+  # Global args
+  local global_args
+  local raw_global_args="$WERCKER_KUBECTL_RAW_GLOBAL_ARGS"
+
+  # token
+  if [ -n "$WERCKER_KUBECTL_TOKEN" ]; then
+    global_args="$global_args --token=\"$WERCKER_KUBECTL_TOKEN\""
+  fi
+
+  # username
+  if [ -n "$WERCKER_KUBECTL_USERNAME" ]; then
+    global_args="$global_args --username=\"$WERCKER_KUBECTL_USERNAME\""
+  fi
+
+  # password
+  if [ -n "$WERCKER_KUBECTL_PASSWORD" ]; then
+    global_args="$global_args --password=\"$WERCKER_KUBECTL_PASSWORD\""
+  fi
+
+  # server
+  if [ -n "$WERCKER_KUBECTL_SERVER" ]; then
+    global_args="$global_args --server=\"$WERCKER_KUBECTL_SERVER\""
+  fi
+
+  # insecure-skip-tls-verify
+  if [ -n "$WERCKER_KUBECTL_INSECURE_SKIP_TLS_VERIFY" ]; then
+    global_args="$global_args --insecure-skip-tls-verify=\"$WERCKER_KUBECTL_INSECURE_SKIP_TLS_VERIFY\""
+  fi
+
+  local retries
+  if [ -n "$WERCKER_KUBE_DEPLOY_RETRIES" ]; then
+    retries=$WERCKER_KUBE_DEPLOY_RETRIES
+  else
+    retries=5
+  fi
+
+  local kubectl="$WERCKER_STEP_ROOT/kubectl $global_args $raw_global_args"
+  local deployment=$WERCKER_KUBE_DEPLOY_DEPLOYMENT
+  local tag=$WERCKER_KUBE_DEPLOY_TAG
+  local deployment_script=$($kubectl get deployment/$deployment -o yaml)
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "deployment_script: " && printf "$deployment_script" && echo ""
+  local current_tag=$(printf "$deployment_script" | grep 'image: ' | cut -d : -f 4)
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "current_tag: " && echo "$current_tag"
+
+  if [[ $current_tag = $tag ]]; then
+    echo "Already running: $tag"
+#    exit
+  fi
+
+  local deployment_script_update=$(printf "$deployment_script" | sed "s,\(image: .*\):.*$,\1:$tag,")
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "deployment_script_update: " && printf "$deployment_script_update" && echo ""
+  local replicas=$(printf "$deployment_script" | grep 'replicas: ' | awk '{print $2}')
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "replicas: $replicas"
+  local minReadySeconds=$(printf "$deployment_script" | grep 'minReadySeconds: ' | awk '{print $2}')
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "minReadySeconds: $minReadySeconds"
+  local strategy=$(printf "$deployment_script" | grep 'strategy: ' | awk '{print $2}')
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "strategy: $strategy"
+  [ -z "$minReadySeconds" ] && minReadySeconds=0
+  local cmd_update='printf "$deployment_script_update" | $kubectl replace -f -'
+  local cmd_rollback="$kubectl rollout undo deployment/$deployment"
+
+  echo "Updating..."
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && printf "$cmd_update"
+  eval $cmd_update
+
+  local deployment_script_now
+  local gen_prev=0
+  local gen_now=1
+  local timeout=$(($minReadySeconds + 10))
+  local total_timeout
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "timeout: $timeout"
+  if [ "$strategy" != "Recreate" ]; then
+    echo "multiplying timeout with # of replicas"
+    total_timeout=`expr "$timeout * $replicas" | bc`
+  fi
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "total_timeout: $total_timeout"
+  while ([ "$gen_prev" != "$gen_now" ] && [ "$retries" !=  "0" ]); do
+    $info "Checking status of deployment:"
+    sleep $total_timeout
+    deployment_script_now=$($kubectl get deployment/$deployment -o yaml)
+    gen_prev=$(printf "$deployment_script_now" | grep 'generation: ' | awk '{print $2}')
+    gen_now=$(printf "$deployment_script_now" | grep 'observedGeneration: ' | awk '{print $2}')
+    [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "prev[$gen_prev] = now[$gen_now]"
+    retries=$retries-1
+  done
+
+  local unavailable=$($kubectl describe deployments | grep 'unavailable' | awk '{print $11}')
+  [ "$WERCKER_KUBECTL_DEBUG" = "true" ] && echo "unavailable: $unavailable"
+  if [ "$unavailable" != "0" ]; then
+    $info "some pods found to be unavailable, rolling back to version: $gen_prev"
+    $cmd_rollback
+  fi
+}
+
+display_version() {
+  $info "Running kubectl version:"
+  $WERCKER_STEP_ROOT/kubectl version -c
+  echo ""
+}
+
+main;
